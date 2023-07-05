88949cbbc3c6f2a34c96f1f1f7f9977a75197205
Ngarotosto <carolvsreyesz@gmail.com>
2023-07-04 19:28:12 -0700

    fix: script re-written so that extracted values are printed using each

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+results = log_line.scan(/\[from:(.*?)\].*?\[to:(.*?)\].*?\[flags:(.*?)\]/)
+results.each do |sender, receiver, flags|
+  puts "#{sender},#{receiver},#{flags}"
+  end

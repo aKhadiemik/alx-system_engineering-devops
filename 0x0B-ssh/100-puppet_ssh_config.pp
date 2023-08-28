9e08437c4f61c4e0add56af19cd405499183aedb
Ngarotosto <carolvsreyesz@gmail.com>
2023-08-28 09:37:09 -0700

    feat: step for regular expression match added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,19 @@
 include stdlib
 
 file_line { 'Fail to authenticate when password supplied':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  ensure 	     => present,
+  path   	     => '/etc/ssh/ssh_config',
+  line   	     => 'PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
 file_line { 'Private key for passwordless authentication':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+  ensure  	     => present,
+  path    	     => '/etc/ssh/ssh_config',
+  line    	     => 'IdentityFile ~/.ssh/school'
+  match   	     => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace 	     => true,
+  append_on_no_match => true 
 }

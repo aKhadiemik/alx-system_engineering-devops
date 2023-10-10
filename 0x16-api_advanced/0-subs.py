c0c0428386dbfd0417e44b76a9c6e6ae75a5b06c
Ngarotosto <carolvsreyesz@gmail.com>
2023-10-11 02:34:32 +0300

    add: Module returns the total number of subscribers for given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module gets number of subreddit subscribers through Reddit API 
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function returns total number of subscribers 
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Reddit API test'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent, allow_redirects=False)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0

a3838110d97276301b6356fd7a1dade1fe0fd587
Ngarotosto <coagengo@gmail.com>
2023-10-20 05:57:33 +0300

    add: solutions to Web Debugging Tasks #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Increasing number of worker processes to eliminate failed requests
+exec { 'Higher limit':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}

a78f1d181fa465bc10639e09ccfc0abf045d18be
Ngarotosto <carolvsreyesz@gmail.com>
2023-10-22 04:37:35 +0300

    fix: manifest modified for direct edit to ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,15 @@
-# Increasing number of worker processes to eliminate failed requests
-exec { 'Higher limit':
-  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/g" /etc/default/nginx',
-  provider => shell
+# fix a Nginx server that was getting many failed requests
+exec {'increase ulimit':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'sed -i "s/-n 15/-n 30000/g" /etc/default/nginx'
 }
 
-exec { 'Restart server':
-  command  => 'service nginx restart',
-  provider => shell,
-  require  => Exec['ulimit']
+exec {'add equal sign to assign value':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'sed -i "s/ulimit $ULIMIT/ulimit=$ULIMIT/g" /etc/init.d/nginx'
+}
+
+exec {'reload and restart Nginx':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'service nginx reload; service nginx restart'
 }

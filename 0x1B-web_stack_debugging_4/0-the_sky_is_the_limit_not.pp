1f6a963004f55a124102cf080d033ecbab9bfc40
Ngarotosto <carolvsreyesz@gmail.com>
2023-10-22 03:57:59 +0300

    fix: edit to script to increase amt of traffic server can handle

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Increasing number of worker processes to eliminate failed requests
 exec { 'Higher limit':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  #command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin:/bin/']
 }
